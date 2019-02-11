281c501584d045113ae94c315a4587c6487b3121
Thornhill-GYL <Thornhill-GYL@outlook.com>
2019-02-11 16:02:50 +0800

    the basic

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name    = 'nester',
+    version = '1.0.0',
+    py_modules =['nester'],
+    author = 'thornhill geng',
+    author_email = 'thornhillgyl@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description= 'a simple printer of nested lists',
+)
